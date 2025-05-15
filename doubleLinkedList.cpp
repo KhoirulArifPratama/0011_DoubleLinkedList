6e477c66be6677cf1c0338a693e2da9219638bb0
KhoirulArifPratama <khoirularif932@gmail.com>
2025-05-15 10:32:25 +0700

    public

--- doubleLinkedList.cpp
+++ doubleLinkedList.cpp
@@ -1,3 +1,33 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class Node
+{
+public:
+    int nimMhs;
+    Node *next;
+    Node *prev;
+};
+
+class DoubleLinkedList
+{
+private:
+    Node *START;
+
+public:
+    DoubleLinkedList()
+    {
+        START = NULL;
+    }
+
+    void addNode()
+    {
+        int nim;
+        string nm;
+        cout << "\nEnter the roll number of the student";
+        cin >> nim;
+
+        Node *newNode = new Node();
+    }
+};
